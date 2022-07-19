3b8db7e03fe6a7643acb21973a99390d515df6ad
alpha <alphamodeyt@gmail.com>
2022-07-18 19:30:28 -0500

    Fully fix armor rendering

--- src/main/java/slimeknights/tconstruct/shared/TinkerClient.java
+++ src/main/java/slimeknights/tconstruct/shared/TinkerClient.java
@@ -2,17 +2,10 @@ package slimeknights.tconstruct.shared;
 
 import io.github.fabricators_of_create.porting_lib.event.client.ModelsBakedCallback;
 import io.github.fabricators_of_create.porting_lib.event.common.RecipesUpdatedCallback;
-import io.github.fabricators_of_create.porting_lib.util.ArmorTextureItem;
 import io.github.fabricators_of_create.porting_lib.util.FluidAttributes;
 import me.alphamode.star.client.renderers.UpsideDownFluidRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.ArmorRenderer;
-import net.minecraft.client.model.Model;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeManager;
 import slimeknights.tconstruct.common.recipe.RecipeCacheInvalidator;
 import slimeknights.tconstruct.fluids.FluidClientEvents;
@@ -27,18 +20,12 @@ import slimeknights.tconstruct.library.client.data.spritetransformer.RecolorSpri
 import slimeknights.tconstruct.smeltery.SmelteryClientEvents;
 import slimeknights.tconstruct.tables.TableClientEvents;
 import slimeknights.tconstruct.tables.client.PatternGuiTextureLoader;
-import slimeknights.tconstruct.tools.TinkerTools;
 import slimeknights.tconstruct.tools.ToolClientEvents;
 import slimeknights.tconstruct.tools.client.ModifierClientEvents;
-import slimeknights.tconstruct.tools.client.SlimelytraArmorModel;
-import slimeknights.tconstruct.tools.client.SlimeskullArmorModel;
 import slimeknights.tconstruct.tools.client.ToolRenderEvents;
-import slimeknights.tconstruct.tools.item.ArmorSlotType;
 import slimeknights.tconstruct.world.WorldClientEvents;
 
 import java.util.function.Consumer;
-
-import static net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer.ARMOR_LOCATION_CACHE;
 
 /**
  * This class should only be referenced on the client side
@@ -72,49 +59,9 @@ public class TinkerClient implements ClientModInitializer {
     ToolClientEvents.clientSetupEvent();
     WorldClientEvents.clientSetup();
 
-    ArmorRenderer elytraRenderer = ((matrices, vertexConsumers, itemStack, entityLiving, armorSlot, light, _default) -> {
-//      if(elytraModel == null)
-      Model elytraModel = SlimelytraArmorModel.getModel(entityLiving, itemStack, _default);
-      ArmorRenderer.renderPart(matrices, vertexConsumers, light, itemStack, elytraModel, getArmorResource(entityLiving, itemStack, armorSlot, null));
-    });
-    ArmorRenderer.register(elytraRenderer, TinkerTools.slimesuit.get(ArmorSlotType.CHESTPLATE));
-    ArmorRenderer skullRenderer = ((matrices, vertexConsumers, itemStack, entityLiving, armorSlot, light, _default) -> {
-//      if(slimeModel == null)
-      Model slimeModel = SlimeskullArmorModel.getModel(itemStack, _default);
-      ArmorRenderer.renderPart(matrices, vertexConsumers, light, itemStack, slimeModel, getArmorResource(entityLiving, itemStack, armorSlot, null));
-    });
-    ArmorRenderer.register(skullRenderer, TinkerTools.slimesuit.get(ArmorSlotType.HELMET));
-
     ModelsBakedCallback.EVENT.register((manager, models, loader) -> {
       FluidAttributes attributes = TinkerFluids.ichor.getStill().getAttributes();
       FluidRenderHandlerRegistry.INSTANCE.register(TinkerFluids.ichor.getStill(), TinkerFluids.ichor.getFlowing(), new UpsideDownFluidRenderer(attributes::getStillTexture, attributes::getFlowingTexture, attributes::getOverlayTexture, attributes.getColor()));
     });
-  }
-
-//  private Model plateModel, travelersModel, elytraModel, slimeModel;
-
-  public ResourceLocation getArmorResource(net.minecraft.world.entity.Entity entity, ItemStack stack, EquipmentSlot slot, @javax.annotation.Nullable String type) {
-    ArmorItem item = (ArmorItem)stack.getItem();
-    String texture = item.getMaterial().getName();
-    String domain = "minecraft";
-    int idx = texture.indexOf(':');
-    if (idx != -1) {
-      domain = texture.substring(0, idx);
-      texture = texture.substring(idx + 1);
-    }
-
-    String s1;
-    if(item instanceof ArmorTextureItem armorTextureItem)
-      s1 = armorTextureItem.getArmorTexture(stack, entity, slot, type);
-    else
-      s1 = String.format("%s:textures/models/armor/%s_layer_%d%s.png", domain, texture, (slot == EquipmentSlot.LEGS ? 2 : 1), type == null ? "" : String.format("_%s", type));
-    ResourceLocation resourcelocation = ARMOR_LOCATION_CACHE.get(s1);
-
-    if (resourcelocation == null) {
-      resourcelocation = new ResourceLocation(s1);
-      ARMOR_LOCATION_CACHE.put(s1, resourcelocation);
-    }
-
-    return resourcelocation;
   }
 }
--- src/main/java/slimeknights/tconstruct/tools/item/SlimeskullItem.java
+++ src/main/java/slimeknights/tconstruct/tools/item/SlimeskullItem.java
@@ -1,31 +1,46 @@
 package slimeknights.tconstruct.tools.item;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import io.github.fabricators_of_create.porting_lib.client.armor.ArmorRenderer;
+import io.github.fabricators_of_create.porting_lib.client.armor.ArmorRendererRegistry;
+import io.github.fabricators_of_create.porting_lib.util.EnvExecutor;
+import io.github.fabricators_of_create.porting_lib.util.client.ClientHooks;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.Model;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import slimeknights.tconstruct.library.tools.definition.ModifiableArmorMaterial;
 import slimeknights.tconstruct.tools.client.SlimeskullArmorModel;
 
-import javax.annotation.Nonnull;
-import java.util.function.Consumer;
-
 /** This item is mainly to return the proper model for a slimeskull */
 public class SlimeskullItem extends SlimesuitItem {
   public SlimeskullItem(ModifiableArmorMaterial material, Properties properties) {
     super(material, ArmorSlotType.HELMET, properties);
+    EnvExecutor.runWhenOn(EnvType.CLIENT, () -> this::initializeClient);
   }
 
-//  TODO: PORT
-//  @Override
-//  public void initializeClient(Consumer<IItemRenderProperties> consumer) {
-//    consumer.accept(new IItemRenderProperties() {
-//      @Nonnull
-//      @Override
-//      public Model getBaseArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlot armorSlot, HumanoidModel<?> _default) {
-//        return SlimeskullArmorModel.getModel(itemStack, _default);
-//      }
-//    });
-//  }
+  @Environment(EnvType.CLIENT)
+  public void initializeClient() {
+    ArmorRendererRegistry.register(new Slimeskull(), this);
+  }
+
+  @Environment(EnvType.CLIENT)
+  private static final class Slimeskull implements ArmorRenderer {
+    @Override
+    public void render(PoseStack matrices, MultiBufferSource vertexConsumers, ItemStack stack, LivingEntity entity, EquipmentSlot slot, int light, HumanoidModel<LivingEntity> contextModel, HumanoidModel<LivingEntity> armorModel) {
+      contextModel.copyPropertiesTo(armorModel);
+      ClientHooks.setPartVisibility(armorModel, slot);
+      Model model =  SlimeskullArmorModel.getModel(stack, armorModel);
+      VertexConsumer vertexconsumer = ItemRenderer.getArmorFoilBuffer(vertexConsumers, RenderType.armorCutoutNoCull(ClientHooks.getArmorResource(entity, stack, slot, null)), false, stack.hasFoil());
+      model.renderToBuffer(matrices, vertexconsumer, light, OverlayTexture.NO_OVERLAY, 1, 1, 1, 1);
+    }
+  }
 }
