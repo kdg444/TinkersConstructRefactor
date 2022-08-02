250e549f4349adf4d7c5b6aa02b94ad0ae99ead5
alpha <alphamodeyt@gmail.com>
2022-08-02 17:01:19 -0500

    wip change to droplets

--- src/main/java/slimeknights/tconstruct/library/recipe/FluidValues.java
+++ src/main/java/slimeknights/tconstruct/library/recipe/FluidValues.java
@@ -2,6 +2,7 @@ package slimeknights.tconstruct.library.recipe;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
 
 /**
  * These values define common amounts of fluids used in recipes.
@@ -12,15 +13,15 @@ import lombok.NoArgsConstructor;
 public final class FluidValues {
   // metal
   /** Value of a single metal ingot, is divisible by 9 */
-  public static final long INGOT = 90;
+  public static final long INGOT = FluidConstants.INGOT;
   /** Value of a single metal nugget */
-  public static final long NUGGET = INGOT / 9;
+  public static final long NUGGET = FluidConstants.NUGGET;
   /** Value of a single metal block, is divisible by 81 */
   public static final long METAL_BLOCK = INGOT * 9;
 
   // gem
   /** Value of a gem such as diamond or emerald */
-  public static final int GEM = 100;
+  public static final int GEM = 8100;
   /** Value of a quarter gem */
   public static final int GEM_SHARD = GEM / 4;
   /** Value of a block of 9 gems, such as emerald or an ender pearl */
@@ -30,19 +31,19 @@ public final class FluidValues {
 
   // clay/brick
   /** Value of a single brick, divisible by 2 */
-  public static final int BRICK = 250;
+  public static final int BRICK = 20250;
   /** Value of a single metal brick block, is divisible by 36 */
   public static final int BRICK_BLOCK = BRICK * 4;
 
   // glass
   /** Value of a single glass block, also used for obsidian */
-  public static final int GLASS_BLOCK = 1000;
+  public static final long GLASS_BLOCK = FluidConstants.BLOCK;
   /** Value of a glass pane, slightly cheaper than vanilla */
-  public static final int GLASS_PANE = GLASS_BLOCK / 4;
+  public static final long GLASS_PANE = GLASS_BLOCK / 4;
 
   // slime
   /** Value of a single slimeball, also used for clay, slime substitutes, and ender pearls */
-  public static final int SLIMEBALL = 250;
+  public static final int SLIMEBALL = 20250;
   /** Value of a block worth 4 slime, see also congealed */
   public static final int SLIME_CONGEALED = SLIMEBALL * 4;
   /** Value of a block worth 9 slime */
@@ -50,11 +51,11 @@ public final class FluidValues {
 
   // soup
   /** Value of a single bowl of soup */
-  public static final int BOWL = 250;
+  public static final int BOWL = 20250;
   /** Value of a single bottle of a potion */
-  public static final int BOTTLE = 250;
+  public static final long BOTTLE = FluidConstants.BOTTLE;
 
   // tank capacities
   /** Capacity of a seared or scorched lantern */
-  public static final int LANTERN_CAPACITY = 50;
+  public static final int LANTERN_CAPACITY = 4050;
 }
