28a93624d03d5a242f2991ba651a71c44e2c3e7d
alpha <alphamodeyt@gmail.com>
2023-11-12 20:58:18 -0600

    Disable broken mantle mixins

--- src/main/java/slimeknights/tconstruct/StarSodiumFixer.java
+++ src/main/java/slimeknights/tconstruct/StarSodiumFixer.java
@@ -20,6 +20,8 @@ public class StarSodiumFixer implements MixinCanceller {
 
   @Override
   public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
+    if (mixinClassName.equals("slimeknights.mantle.mixin.client.FluidStorageMixin") || mixinClassName.equals("slimeknights.mantle.mixin.client.ItemStorageMixin") || mixinClassName.equals("slimeknights.mantle.mixin.common.TransactionManagerImplMixin"))
+      return true;
     if (mixinClassName.startsWith("me.alphamode.star.mixin.sodium") || mixinClassName.startsWith("me.alphamode.star.mixin.indium"))
       return FabricLoader.getInstance().getModContainer("sodium").map(modContainer -> modContainer.getMetadata().getVersion().compareTo(SODIUM_VERSION) < 0).orElse(false);
     return false;
