52dfa71940e73a03d2d29116c43e31046cf1465c
alpha <alphamodeyt@gmail.com>
2023-11-22 17:01:35 -0600

    Update REI to match JEI

--- src/main/java/slimeknights/tconstruct/plugin/rei/transfer/TinkerStationMenuInfo.java
+++ src/main/java/slimeknights/tconstruct/plugin/rei/transfer/TinkerStationMenuInfo.java
@@ -5,6 +5,7 @@ import me.shedaniel.rei.api.common.transfer.info.stack.SlotAccessor;
 import net.minecraft.world.inventory.Slot;
 import slimeknights.tconstruct.plugin.rei.modifiers.ModifierRecipeDisplay;
 import slimeknights.tconstruct.tables.menu.TinkerStationContainerMenu;
+import slimeknights.tconstruct.tools.item.ArmorSlotType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,8 +27,8 @@ public class TinkerStationMenuInfo implements StationMenuInfo<TinkerStationConta
   @Override
   public Iterable<SlotAccessor> getInventorySlots(MenuInfoContext<TinkerStationContainerMenu, ?, ModifierRecipeDisplay> context) {
     List<SlotAccessor> slots = new ArrayList<>();
-    // skip over inputs and the output slot
-    int start = context.getMenu().getInputSlots().size() + 1;
+    // skip over inputs, output slot, tool slot, armor, and offhand
+    int start = context.getMenu().getInputSlots().size() + 3 + ArmorSlotType.values().length;
     for(int i = start; i < start + 36; i++) {
       Slot slot = context.getMenu().getSlot(i);
       slots.add(SlotAccessor.fromSlot(slot));
